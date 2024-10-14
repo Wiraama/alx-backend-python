2c4d2b04dc499e115a7beb9aa4d1b42e6020b6c7
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-14 23:34:53 +0300

    urrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Import wait_random from the previous python file that you’ve written
+and write an async routine called wait_n that takes in 2 int arguments
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n :int, max_delay: int) -> List[float]:
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*tasks)
+    return result
