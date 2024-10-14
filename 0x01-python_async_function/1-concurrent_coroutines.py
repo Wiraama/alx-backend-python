b19fa857715c899dd74d1d2efacde2a4b22ab12a
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-14 23:54:13 +0300

    urrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,14 +4,21 @@ Import wait_random from the previous python file that you’ve written
 and write an async routine called wait_n that takes in 2 int arguments
 """
 import asyncio
+import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ ... """
+    all_list = []
+    delay_list = []
+    for i in range(n):
+        delay = asyncio.create_task(wait_random(max_delay))
+        delay.add_done_callback(lambda x: delay_list.append(x.result()))
+        all_list.append(delay)
 
+    for a in all_list:
+        await a
 
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    result = await asyncio.gather(*tasks)
-    return result
+    return delay_list
