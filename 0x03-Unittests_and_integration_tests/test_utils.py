cb8b6a0b0595a8e7d3005f043816d4926546b416
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-01 22:10:05 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """ test for utils.access_nested_map """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,25 @@ class TestAccessNestedMap(unittest.TestCase):
         inputs (use @parameterized.expand): """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ class and implement the TestGetJson.test_get_json method to test
+    that utils.get_json returns the expected result."""
+    
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """..."""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+
+        mock_get.assert_called_once_with(test_url)
