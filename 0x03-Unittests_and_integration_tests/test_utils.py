8d8371a2eabce05c7cd0e6c49ad6f4688079ae4b
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-08 22:48:46 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,8 +13,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-
-
     def test_access_nested_map(self, nested_map, path, expected):
         """ >>> """
         self.assertEqual(access_nested_map(nested_map, path), expected)
@@ -33,8 +31,6 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
     """ class and implement the TestGetJson.test_get_json method to test
     that utils.get_json returns the expected result."""
-    
-
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
